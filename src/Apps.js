a1e7596f0d68f005cbdf270f30162ba24ef46cd1
Roohan Kebria <rohankebria@gmail.com>
2021-02-24 19:39:49 +0100

    setState to update state object of component

--- src/Apps.js
+++ src/Apps.js
@@ -3,15 +3,32 @@ import React, { Component } from "react";
 class Car extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { color: "red" }; // state is an object
+    this.state = {
+      brand: "KIA",
+      model: "Ceed",
+      color: "red",
+      year: 1964,
+    }; // state is an object
   }
+
+  changeColor = () => {
+    this.setState({ model: "Rio" });
+  };
+
   render() {
     return (
-      <h2>
-        I am a {this.state.color} and {this.props.color} Car, named
-        {" " + this.props.brand}! and my friends is{" "}
-        {this.props.weAll.name + " " + this.props.weAll.model}
-      </h2>
+      <div>
+        <h3>
+          I am a {this.state.color} and {this.props.color} Car, named
+          {" " + this.props.brand}! and my friends are{" "}
+          {this.props.weAll.name + " " + this.props.weAll.model} and{" "}
+          {this.state.brand}
+          {" " + this.state.model}
+        </h3>
+        <button type="button" onClick={this.changeColor}>
+          <h1> Change model </h1>
+        </button>
+      </div>
     );
   }
 }
@@ -20,6 +37,11 @@ class Garage extends React.Component {
     super();
     this.state = { location: "Sigtuna" };
   }
+
+  changeLocation = () => {
+    this.setState({ location: "Märsta" });
+  };
+
   render() {
     // console.log(this.props);
     const carname = "Volvo";
@@ -31,6 +53,9 @@ class Garage extends React.Component {
           Who lives in my {this.state.location} {this.props.type} Garage?
         </h1>
         <Car color="blue" brand={carname} weAll={carinfo} />
+        <button type="button" onClick={this.changeLocation}>
+          <h1> Change location </h1>
+        </button>
       </div>
     );
   }
