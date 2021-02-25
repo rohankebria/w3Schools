9073ddede53d15b724f5dae4bf6e3188b3ca4b78
Roohan Kebria <rohankebria@gmail.com>
2021-02-25 14:33:51 +0100

    React forms and css

--- src/Apps.js
+++ src/Apps.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import "./App.css";
 
 class Car extends React.Component {
   constructor(props) {
@@ -15,8 +16,18 @@ class Car extends React.Component {
 
   shoot(msg, evnt) {
     // event handler
-    console.log(this, evnt); // this is binded inside constructor, no need for Arrow function
-    alert(msg + "---" + this + " type is: " + evnt.type);
+    //console.log(this, evnt); // this is binded inside constructor, no need for Arrow function
+    console.log(
+      //alert(
+
+      msg +
+        "---" +
+        this +
+        " type is: " +
+        evnt.type +
+        " React type is: " +
+        evnt._reactName
+    );
   }
 
   changeModel = () => {
@@ -37,8 +48,15 @@ class Car extends React.Component {
     console.log(pProps, pState);
   }
   render() {
+    const mystyle = {
+      // call with single quote
+      color: "purple",
+      backgroundColor: "lightgreen",
+      padding: "10px",
+      fontFamily: "Arial",
+    };
     return (
-      <div>
+      <div style={mystyle}>
         <h3 onMouseOver={this.shoot.bind(this, "Great Binded Shoot")}>
           I am a {this.state.color} and {this.props.color} Car, named
           {" " + this.props.brand}! and my friends are{" "}
@@ -58,7 +76,13 @@ class Car extends React.Component {
 class Garage extends React.Component {
   constructor() {
     super();
-    this.state = { location: "Sigtuna" };
+    this.state = {
+      location: "Sigtuna",
+      username: "?",
+      age: "?",
+      errormessage: "",
+      description: "The content of a textarea goes in the value attribute",
+    };
   }
 
   changeLocation = () => {
@@ -78,20 +102,62 @@ class Garage extends React.Component {
     return { location: "Sweden" };
   }*/
 
+  myChangeHandler = (event) => {
+    console.log(event);
+    let nam = event.target.name; // jsx elemt name
+    let val = event.target.value; // jsx entered value
+    let letters = /^[A-Za-z]+$/;
+    let err = ""; //local error
+    if (nam === "age") {
+      if (!Number(val)) {
+        //alert("Your age must be a number");
+        err = <strong>Your age must be a number</strong>;
+        val = 0; // age reset to zero !;
+      }
+    }
+
+    if (nam === "username") {
+      if (!val.match(letters)) {
+        //  alert("Your username must be a String");
+        err = <strong>Your username must be a String</strong>;
+        val = "****"; // username reset to "" !;
+      }
+    }
+
+    this.setState({ [nam]: val }); // object de-structuring
+    this.setState({ errormessage: err });
+  };
+  mySubmitHandler = (event) => {
+    event.preventDefault();
+    alert("You are submitting " + this.state.username + this.state.age);
+  };
+
   render() {
     // console.log(this.props);
     const carname = "Volvo";
     const carinfo = { name: "Ford", model: "Mustang" };
 
     return (
-      <div>
+      <div style={{ backgroundColor: "lightblue" }}>
         <h1>
           Who lives in my {this.state.location} {this.props.type} Garage?
         </h1>
         <Car color="blue" brand={carname} weAll={carinfo} />
         <button type="button" onClick={this.changeLocation}>
-          <h1> Change location </h1>
+          <h1 style={{ color: "red", fontSize: "15px" }}> Change location </h1>
         </button>
+        <form onSubmit={this.mySubmitHandler}>
+          <h1>
+            Hello {this.state.username} who is {this.state.age} years old
+          </h1>
+          <p>Enter your name:</p>
+          <input name="username" type="text" onChange={this.myChangeHandler} />
+          <p>Enter your age:</p>
+          <input type="text" name="age" onChange={this.myChangeHandler} />
+          <input type="submit" />
+        </form>
+        {this.state.errormessage}
+        <textarea value={this.state.description} />
       </div>
     );
   }
